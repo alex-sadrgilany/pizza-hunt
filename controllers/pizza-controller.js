970dc1486041e706e9969dcc0e6721c459667d05
AS <asadrgilany7@gmail.com>
2021-12-15 15:36:00 -0500

    added validators to each model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -52,7 +52,8 @@ const pizzaController = {
       },
       body,
       {
-          new: true
+          new: true,
+          runValidators: true
       })
         .then(dbPizzaData => {
             if (!dbPizzaData) {
